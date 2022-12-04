2d747f01e2a40442bfd1791fa2c2832ae407ff8a
Mycroft Kang <thkang0629@gmail.com>
2022-12-04 14:26:29 +0900

    Remove post_migrate signal

--- dot_dynamic_scopes/apps.py
+++ dot_dynamic_scopes/apps.py
@@ -3,12 +3,12 @@ Django app config for the dot-dynamic-scopes package.
 """
 
 from django.apps import AppConfig as BaseAppConfig
-from django.db.models.signals import post_migrate
+# from django.db.models.signals import post_migrate
 
 
 class AppConfig(BaseAppConfig):
     name = 'dot_dynamic_scopes'
 
-    def ready(self):
-        from .signals import register_scopes
-        post_migrate.connect(register_scopes, sender = self)
+    # def ready(self):
+    #     from .signals import register_scopes
+    #     post_migrate.connect(register_scopes, sender = self)
